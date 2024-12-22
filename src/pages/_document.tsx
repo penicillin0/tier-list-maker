55c8d46d4417b15cbb1e021ee8da1a0aebe19795
penicillin0 <ayahitonakamura@gmail.com>
2024-12-22 17:21:51 +0900

    Headコンポーネントにfaviconとmetaタグを追加し、文字セットとビューポートを設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
