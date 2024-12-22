1b3987ecb66444f426e261f21e1ea0c676c49021
penicillin0 <ayahitonakamura@gmail.com>
2024-12-22 17:06:35 +0900

    Headコンポーネントを追加し、タイトルを設定。_document.tsxからHeadを削除。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import TierList from '@/components/TierList'
 import styles from '@/styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Tier List Manager</h1>
-      <p>Tier表メーカー</p>
-      <TierList />
-    </div>
+    <>
+      <Head>
+        <title>Tier表作成ツール | 簡単で便利な無料Webアプリ</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Tier List Manager</h1>
+        <p>Tier表メーカー</p>
+        <TierList />
+      </div>
+    </>
   )
 }
